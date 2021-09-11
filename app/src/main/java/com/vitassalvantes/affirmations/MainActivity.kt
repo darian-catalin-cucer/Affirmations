78dfe5c334dbb2c5e91975bf2c829e0fbb012a2b
VitasSalvantes <salvantesvitas@gmail.com>
2021-09-11 15:00:02 +0200

    Added adapter for recycler view

--- app/src/main/java/com/vitassalvantes/affirmations/MainActivity.kt
+++ app/src/main/java/com/vitassalvantes/affirmations/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vitassalvantes.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vitassalvantes.affirmations.adapter.ItemAdapter
 import com.vitassalvantes.affirmations.data.Datasource
 import com.vitassalvantes.affirmations.databinding.ActivityMainBinding
 
@@ -12,5 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Initialize data
+        val myDataset = Datasource().loadAffirmations()
+        binding.recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        binding.recyclerView.setHasFixedSize(true)
     }
 }
--- app/src/main/java/com/vitassalvantes/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/vitassalvantes/affirmations/adapter/ItemAdapter.kt
@@ -1,16 +1,50 @@
 package com.vitassalvantes.affirmations.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.vitassalvantes.affirmations.R
+import com.vitassalvantes.affirmations.databinding.ActivityMainBinding
+import com.vitassalvantes.affirmations.databinding.ListItemBinding
 import com.vitassalvantes.affirmations.model.Affirmation
 
-class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
+/**
+ * Adapter for the [RecyclerView] in [MainActivity]. Displays [Affirmation] data object.
+ */
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.item_title)
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just an Affirmation object.
+    class ItemViewHolder(val binding: ListItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    /**
+     * Create new views (invoked by the layout manager)
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // create a new view
+        val binding =
+            ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemViewHolder(binding)
     }
+
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     */
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.binding.itemTitle.text = context.resources.getString(item.stringResourceId)
+    }
+
+    /**
+     * Return the size of your dataset (invoked by the layout manager)
+     */
+    override fun getItemCount() = dataset.size
 
 }
